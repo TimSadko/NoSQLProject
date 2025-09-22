53395f849e0459bddc596743d8e7d8d8835acef8
tim.sadko@gmail.com <tim.sadko@gmail.com>
2025-09-22 11:54:59 +0200

    Added advanced Employee view in Employes/Index

--- NoSQLProject/Models/Employee.cs
+++ NoSQLProject/Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace NoSQLProject.Models
 {
@@ -21,11 +22,11 @@ namespace NoSQLProject.Models
         private string _password = "";
         private Employee_Roles _role = 0;
         private Employee_Status _status = 0;
-        private List<Employee>? _managed_employees = null;
+        private List<string>? _managed_employees_id = null;
 
         public Employee() { }
 
-        public Employee(string id, string first_name, string last_name, string email, string password, Employee_Roles role, Employee_Status status, List<Employee>? managed_employees)
+        public Employee(string id, string first_name, string last_name, string email, string password, Employee_Roles role, Employee_Status status, List<string>? managed_employees_id)
         {
             _id = id;
             _first_name = first_name;
@@ -34,7 +35,7 @@ namespace NoSQLProject.Models
             _password = password;
             _role = role;
             _status = status;
-            _managed_employees = managed_employees;
+            _managed_employees_id = managed_employees_id;
         }
 
         [BsonId]
@@ -42,24 +43,32 @@ namespace NoSQLProject.Models
         public string? Id { get => _id; set => _id = value; }
 
         [BsonElement("first_name")]
+        [JsonPropertyName("first_name")]
         public string FirstName { get => _first_name; set => _first_name = value; }
 
         [BsonElement("last_name")]
+        [JsonPropertyName("last_name")]
         public string LastName { get => _last_name; set => _last_name = value; }
 
         [BsonElement("email")]
+        [JsonPropertyName("email")]
         public string Email { get => _email; set => _email = value; }
 
         [BsonElement("password")]
+        [JsonPropertyName("password")]
         public string Password { get => _password; set => _password = value; }
 
         [BsonElement("role")]
+        [JsonPropertyName("role")]
         public Employee_Roles Role { get => _role; set => _role = value; }
 
         [BsonElement("status")]
+        [JsonPropertyName("status")]
         public Employee_Status Status { get => _status; set => _status = value; }
 
         [BsonElement("managed_employees")]
-        public List<Employee>? ManagedEmployees { get => _managed_employees; }
+        [JsonPropertyName("managed_employees")]
+        //[BsonRepresentation(MongoDB.Bson.BsonType.Array)]
+        public List<string>? ManagedEmployeesId { get => _managed_employees_id; set => _managed_employees_id = value; }
     }
 }
