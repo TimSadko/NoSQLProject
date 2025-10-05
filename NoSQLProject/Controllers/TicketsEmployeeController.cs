6b0e47e9a4113639a49730bc88b7a7343997ac10
Abdullah SEN <sen-abdullah@yandex.com>
2025-10-05 21:43:53 +0200

    Add tickets overview for employee

---
+++ NoSQLProject/Controllers/TicketsEmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoSQLProject.Models;
+using NoSQLProject.Other;
+using NoSQLProject.Repositories;
+using NoSQLProject.ViewModels;
+
+namespace NoSQLProject.Controllers
+{
+    public class TicketsEmployeeController(ITicketRepository repository) : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var authenticatedEmployee = Authenticate();
+            if (authenticatedEmployee?.Id == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var tickets = await repository.GetAllByEmployeeIdAsync(authenticatedEmployee.Id);
+            var employeeTickets = new EmployeeTickets(tickets, authenticatedEmployee);
+            return View(employeeTickets);
+        }
+
+        public Employee? Authenticate()
+        {
+            var employee = Authorization.GetLoggedInEmployee(HttpContext);
+            return employee is null or ServiceDeskEmployee ? null : employee;
+        }
+    }
+}
--- NoSQLProject/Repositories/TicketRepository.cs
+++ NoSQLProject/Repositories/TicketRepository.cs
@@ -18,6 +18,11 @@ namespace NoSQLProject.Repositories
             return await _tickets.FindAsync(new BsonDocument()).Result.ToListAsync();
         }
 
+        public async Task<List<Ticket>> GetAllByEmployeeIdAsync(string id)
+        {
+            return await _tickets.FindAsync(Builders<Ticket>.Filter.Eq("created_by", id)).Result.ToListAsync();
+        }
+
         public async Task<Ticket?> GetByIdAsync(string id)
         {
             return await _tickets.FindAsync(Builders<Ticket>.Filter.Eq("_id", ObjectId.Parse(id))).Result.FirstOrDefaultAsync();
